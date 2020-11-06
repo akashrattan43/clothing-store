f14b0ca411be8667376d19d044e934e0a120ea95
akash rattan <akashrattan43@Gmail.com>
2020-11-05 19:58:31 -0800

    route added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 import Homepage from './pages/homepage/homepage.component'
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
-      <Homepage />
+    <Switch>
+      <Route path='/' component={Homepage} />
+      <Route path='/hats' component={HatsPage} />
+      {/* <Homepage /> */}
+    </Switch>
     </div>
   );
 }
